22843bc217448d4b0cb67913101ac37b1c07b108
Son Tran <54421820+transon89@users.noreply.github.com>
2019-12-11 00:00:23 -0600

    fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    //click listeners to activate the functions and Calls
+
     $("#search-btn").click(weatherNow);
     $("#search-btn").click(fiveDayForecast);
     $("#cityList").click(weatherNow);
@@ -9,71 +9,70 @@ $(document).ready(function () {
     let time = moment().format("LL");
     let myKey = "751635dd458149957afa00a64308bc08";
     $(".figure").css("display", "none");
-    // $("#search-container").css("left", "320px");
+ 
     $(".ul-container").css("display", "none");
 
-    //Function to call current weather  
+
     function weatherNow(event) {
         event.preventDefault();
-        //will target the value of the user userInput or the saved list
+
         if ($(this).attr("id") === "cityList") {
             let x = event.target;
             userInput = $(x).text();
             console.log(userInput);
         } else {
-            userInput = $(this).prev().val(); //getting value of user input
+            userInput = $(this).prev().val(); 
         }
-        $(".figure").empty(); //empty search results upon each new search
+        $(".figure").empty(); 
         $("#search-container").animate({ "left": "10px"}, 600);
         $(".ul-container").css("display", "flex");
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&APPID=" + myKey;
-        //calling the API
+
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
             $(".figure").css("display", "block");
-            //setting the values to a new DOM element 
+ 
             let city = $("<h1>").addClass("city-name").text(`City: ${response.name}`);
             let date = $("<h3>").addClass("date").text(`Date: ${time}`);
             let iconImage = $("<img>").addClass("icon-image").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
-            let tempF = parseInt((response.main.temp - 273.15) * 1.8 + 32); //kelvin to farenheight Conversion
+            let tempF = parseInt((response.main.temp - 273.15) * 1.8 + 32); 
             let temperature = $("<h4>").addClass("current-temp").text(`Current Temperature: ${tempF} F°`);
             let humidity = $("<h4>").addClass("humidity").text(`Humidity: ${response.main.humidity}%`);
             let windSpeed = $("<h4>").addClass("wind-speed").text(`Wind Speed ${response.wind.speed} mph`);
-            //Appending the values to the figure box
+
             $(".figure").append(city, iconImage, date, temperature, humidity, windSpeed);
 
         })
 
     }
-    //end current day call begin Five day forecast call
+
     function fiveDayForecast() {
         if ($(this).attr("id") === "cityList") {
             let x = event.target;
             userInput = $(x).text();
             console.log(userInput);
         } else {
-            userInput = $(this).prev().val(); //getting value of user input
+            userInput = $(this).prev().val(); 
         }
         let dayDisplay = 1;
         let fiveDayCall = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&APPID=" + myKey;
-        //calling the 5 day forecast
+
         $.ajax({
             url: fiveDayCall,
             method: "GET"
         }).then(function (response) {
             console.log(response);
             let listArray = response.list;
-            listArray.forEach(element => {   //use for each method to loop through object list
-                //   console.log(element);
+            listArray.forEach(element => {   
+     ;
                 let yearDateTime = element.dt_txt;
-                //    console.log (yearDatetime);    
-                let currentDate = yearDateTime.split(" ")[0]; //splitting the full date
-                let currentTime = yearDateTime.split(" ")[1]; //and time  in the object
-                
-                //getting a specific time of day to pull data from and insert inot the DOM
+ 
+                let currentDate = yearDateTime.split(" ")[0];
+                let currentTime = yearDateTime.split(" ")[1]; 
+
                 if (currentTime === "15:00:00") {
                     let day = currentDate.split("-")[2];
                     let month = currentDate.split("-")[1];
@@ -89,7 +88,7 @@ $(document).ready(function () {
         })
     }
 
-    //-------------------------Local Storage-----------//
+
     let ul = $("#cityList");
     let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
     let data = JSON.parse(localStorage.getItem('items'));
